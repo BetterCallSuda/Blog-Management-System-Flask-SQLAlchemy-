8d86aa982e0beb2da45ba3459345320140a5472e
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-02-24 11:06:05 +0530

    Update models.py

--- models.py
+++ models.py
@@ -18,3 +18,41 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+
+    def to_dict(self):
+            return {
+                "id": self.id,
+                "username": self.username,
+                "email": self.email
+            }
+
+
+# -----------------------------
+# POST MODEL
+# -----------------------------
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    author = db.relationship("User", back_populates="posts")
+
+    comments = db.relationship("Comment", back_populates="post", cascade="all, delete")
+
+    def __repr__(self):
+        return f"<Post {self.title}>"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "author": self.author.username,
+            "created_at": self.created_at,
+            "comments": [comment.to_dict() for comment in self.comments]
+        }
